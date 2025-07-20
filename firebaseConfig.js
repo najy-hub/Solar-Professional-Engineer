eb1f6ef4541dde4320c1785bf5a4ed02bc1af2cf
najy-hub <solarpowerplantprofessionaleng@gmail.com>
2025-07-20 14:28:17 +0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBWdrUtLP4fVKHDmQDC6nUJzBMPA7AmbMc",
+  authDomain: "solar-engineer-43cbc.firebaseapp.com",
+  projectId: "solar-engineer-43cbc",
+  storageBucket: "solar-engineer-43cbc.firebasestorage.app",
+  messagingSenderId: "816573344174",
+  appId: "1:816573344174:web:b472b052d26b7be1d7003f",
+  measurementId: "G-Q52GKBHGML"
+};
